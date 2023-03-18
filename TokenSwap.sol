ae484c7200223ede0d544db2005e072d441f65f2
Balamurugan <117345962+balagicrypto@users.noreply.github.com>
2023-03-18 10:57:41 +0530

    Add files via upload

---
+++ TokenSwap.sol
@@ -0,0 +1,67 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/IERC20.sol";
+
+/*
+How to swap tokens
+
+1. Bala has 100 tokens from GiCoin, which is a ERC20 token.
+2. Suresh has 100 tokens from BooCoin, which is also a ERC20 token.
+3. Bala and Suresh wants to trade 10 GiCoin for 20 BooCoin.
+4. Bala or suresh deploys TokenSwap
+5. Bala approves TokenSwap to withdraw 10 tokens from GiCoin
+6. Suresh approves TokenSwap to withdraw 20 tokens from BooCoin
+7. Bala or suresh calls TokenSwap.swap()
+8. Bala and suresh traded tokens successfully.
+*/
+contract TokenSwap {
+    IERC20 public token1;
+    address public owner1;
+    uint public amount1;
+    IERC20 public token2;
+    address public owner2;
+    uint public amount2;
+
+    constructor(
+        address _token1,
+        address _owner1,
+        uint _amount1,
+        address _token2,
+        address _owner2,
+        uint _amount2
+    ) {
+        token1 = IERC20(_token1);
+        owner1 = _owner1;
+        amount1 = _amount1;
+        token2 = IERC20(_token2);
+        owner2 = _owner2;
+        amount2 = _amount2;
+    }
+
+    function swap() public {
+        require(msg.sender == owner1 || msg.sender == owner2, "Not authorized");
+        require(
+            token1.allowance(owner1, address(this)) >= amount1,
+            "Token 1 allowance too low"
+        );
+        require(
+            token2.allowance(owner2, address(this)) >= amount2,
+            "Token 2 allowance too low"
+        );
+
+        _safeTransferFrom(token1, owner1, owner2, amount1);
+        _safeTransferFrom(token2, owner2, owner1, amount2);
+    }
+
+    function _safeTransferFrom(
+        IERC20 token,
+        address sender,
+        address recipient,
+        uint amount
+    ) private {
+        bool sent = token.transferFrom(sender, recipient, amount);
+        require(sent, "Token transfer failed");
+    }
+}
+
